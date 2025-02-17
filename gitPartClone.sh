84e7921794475ac1f8ebdc17f3ee46a3fbfa162b
Upadhyayula <sriman.upadhyayula@intel.com>
2025-02-17 01:20:01 -0800

    Narrowed down iVal Cloning

--- gitPartClone.sh
+++ gitPartClone.sh
@@ -5,6 +5,6 @@ mkdir PartRepo
 cd PartRepo
 git clone --filter=blob:none --origin upstream --sparse https://ATTD@dev.azure.com/ATTD/HDMT_Prod/_git/HDMTOS -b $branch --depth $(($d + 1))
 cd HDMTOS
-git sparse-checkout set I2L TAL HAL Validation/iVal
+git sparse-checkout set I2L TAL HAL Validation/iVal/HBI Validation/iVal/HDMT Validation/iVal/ValidationCode/TestClasses Validation/iVal/ValidationCode/UserFunctions Validation/iVal/BuildScripts
 git diff --name-only HEAD~$d > ../../changedFileName.txt
 cd ..
